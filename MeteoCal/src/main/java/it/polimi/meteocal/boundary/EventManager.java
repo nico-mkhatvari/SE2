c60106149b1049ce782f1aa486c76b2c83fc8b64
jiasheng89 <jiasheng89@gmail.com>
2015-01-09 20:30:15 +0100

    added edit functionality

--- MeteoCal/src/main/java/it/polimi/meteocal/boundary/EventManager.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/boundary/EventManager.java
@@ -1,50 +1,115 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this teventEjbplate file, choose Tools | TeventEjbplates
+ * and open the teventEjbplate in the editor.
  */
 package it.polimi.meteocal.boundary;
 
+import it.polimi.meteocal.control.EventEJB;
 import it.polimi.meteocal.entity.Event;
+import java.util.Date;
 import java.util.List;
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
 
 /**
  *
  * @author jiasheng
  */
-@Stateless
-public class EventManager extends AbstractFacade<Event> {
-    
-    @PersistenceContext(unitName = "it.polimi_MeteoCal_war_1.0-SNAPSHOTPU")
-    private EntityManager em;
-    
-    
-    @Override
-    protected EntityManager getEntityManager() {
-        return em;
-    }
+@ManagedBean
+@Named(value = "eventBean")
+@RequestScoped
+public class EventManager {
+
+    @EJB
+    private EventEJB eventEjb;
+    private Event event;
+    private Event selectedEvent;
+    private Date currentDate;
+    private List<Event> eventlist;    
+   // private Boolean notPrima = false;   
+    private static int passedParameter;
 
     public EventManager() {
-        super(Event.class);
     }
 
-    public void save(Event event) {
-        em.persist(event);
+    public Event getEvent() {
+        if (event == null) {
+            event = new Event();
+        }
+        return event;
     }
 
-    public List<Event> findEvents() {
-        TypedQuery<Event> query = em.createNamedQuery("Event.findAll", Event.class);
-        return query.getResultList();
+    public void setEvent(Event event) {
+        this.event = event;
     }
 
-    public void deleteEvent(int id) {     
-        
-        Event e = em.find(Event.class, id);
-        em.remove(e);
-        
+    public List<Event> getEventlist() {
+        return eventlist;
+    }
+
+    public void setEventlist(List<Event> eventlist) {
+        this.eventlist = eventlist;
+    }
+
+    public Event getSelectedEvent() {
+        return selectedEvent;
+    }
+
+    public void setSelectedEvent(Event selectedEvent) {
+        this.selectedEvent = selectedEvent;
+    }
+
+    public void loadEvent() {
+       // if (notPrima == false) {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            passedParameter = Integer.parseInt((facesContext.getExternalContext().getRequestParameterMap().get("id")));
+            selectedEvent = eventEjb.findEvent(passedParameter);
+       //     notPrima = true;
+      //  }
+    }
+
+    public Date getCurrentDate() {
+        if (currentDate == null) {
+            currentDate = new Date();
+        }
+        return currentDate;
+    }
+
+    public String create() {
+        eventEjb.save(event);
+        eventlist = eventEjb.findEvents();
+        return "view";
+    }
+
+    @PostConstruct
+    public void init() {
+        eventlist = eventEjb.findEvents();
+    }
+
+    public String delete(int id) {
+        try {
+            eventEjb.deleteEvent(id);
+            eventlist = eventEjb.findEvents();
+            return "view";
+        } catch (EJBException e) {
+            System.out.println(e);
+        }
+        return "#";
+    }
+
+    public String editPage(int id) {
+        return "edit?faces-redirect=true&id=" + id;
+    }
+
+    public String edit(Date date) {
+        eventEjb.update(passedParameter, date);
+        eventlist = eventEjb.findEvents();
+        return "view";
     }
 }
