2b4e9cc46dc91958ea940a74fa07d17c6ebfa142
Nico Mkhatvari <nico.mkhatvari@gmail.com>
2015-01-21 22:34:20 +0100

    Timer pulito

--- MeteoCal/src/main/java/CalendarTimers/CalendarTimer.java
+++ MeteoCal/src/main/java/CalendarTimers/CalendarTimer.java
@@ -7,14 +7,8 @@ package CalendarTimers;
 
 import it.polimi.meteocal.control.EventsEJB;
 import it.polimi.meteocal.entity.Events;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-<<<<<<< HEAD
-=======
-
->>>>>>> 8aaee5951e71bf311e94879b2a7df55577ec3c59
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Startup;
@@ -27,43 +21,38 @@ import javax.persistence.TemporalType;
  *
  * @author terminator
  */
+
+
 @Startup
 @Singleton
 
 public class CalendarTimer {
-    
+
     @PersistenceContext
     EntityManager em;
     @EJB
     private EventsEJB eventEjb;
-    
-    
-    Calendar addXhours(int hours){
-        
+
+    Calendar addXhours(int hours) {
+
         Calendar date = Calendar.getInstance();
         date.add(Calendar.HOUR_OF_DAY, hours);
         return date;
-        
+
     }
-    
-    
+
     @Schedule(second = "*/30", minute = "*", hour = "*", persistent = false)
-    public void deleteOldEvents(){
+    public void deleteOldEvents() {
         int i = 0;
-        
-       
-<<<<<<< HEAD
-        List<Events> expiredEvents = em.createNamedQuery("Events.expiredEvents", Events.class).setParameter(1, new Date(), TemporalType.TIMESTAMP).getResultList();
-        List<Events> events24 = em.createNamedQuery("Events.expiredEvents", Events.class).setParameter(1, new Date(), TemporalType.TIMESTAMP).getResultList();
-=======
-        List<Events> expiredEvents = em.createNamedQuery("Events.expiredEvents", Events.class).setParameter("enddate", mydate, TemporalType.TIMESTAMP).getResultList();
-        
->>>>>>> 8aaee5951e71bf311e94879b2a7df55577ec3c59
-                
-        while(expiredEvents.iterator().hasNext()){
-            eventEjb.deleteEvent(expiredEvents.get(i).getId()); //si può get(i++)??
+
+        List<Events> expiredEvents = em.createNamedQuery("Events.expiredEvents", Events.class).setParameter("enddate", addXhours(0), TemporalType.TIMESTAMP).getResultList();
+        List<Events> event24 = em.createNamedQuery("Events.expiredEvents", Events.class).setParameter("enddate", addXhours(24), TemporalType.TIMESTAMP).getResultList();
+        List<Events> event72 = em.createNamedQuery("Events.expiredEvents", Events.class).setParameter("enddate", addXhours(72), TemporalType.TIMESTAMP).getResultList();
+
+        while (expiredEvents.iterator().hasNext()) {
+            eventEjb.deleteEvent(expiredEvents.get(i).getId());
             i++;
         }
     }
-    
+
 }
